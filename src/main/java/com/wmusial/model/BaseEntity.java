84caa67b15e313a907183c2e95883c50a3f34255
Wojciech Musiał <wmusial@diib.com>
2016-06-30 14:02:26 +0200

    add lombok project

--- src/main/java/com/wmusial/model/BaseEntity.java
+++ src/main/java/com/wmusial/model/BaseEntity.java
@@ -1,18 +1,18 @@
 package com.wmusial.model;
 
+import lombok.Data;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Data
 public class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
 }
--- src/main/java/com/wmusial/model/User.java
+++ src/main/java/com/wmusial/model/User.java
@@ -1,11 +1,18 @@
 package com.wmusial.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "user")
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class User extends BaseEntity {
 
     @Column(name = "first_name")
@@ -20,35 +27,4 @@ public class User extends BaseEntity {
     @Column(name = "password")
     private String password;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
